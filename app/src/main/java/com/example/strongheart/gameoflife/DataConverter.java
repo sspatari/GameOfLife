7a75a76825d35ecff3c13caa01911be05dc7214b
Alexei Sersun <alex.sersun@yahoo.com>
2017-06-17 23:55:52 +0300

    Set terrain's parameters in `DataConverter` constructor.

--- app/src/main/java/com/example/strongheart/gameoflife/DataConverter.java
+++ app/src/main/java/com/example/strongheart/gameoflife/DataConverter.java
@@ -3,53 +3,31 @@ package com.example.strongheart.gameoflife;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by the-french-cat on 17/06/17.
  */
 
-public class DataConverter implements DataConvertable {
+class DataConverter implements DataConvertable {
+    private Double mFieldLengthX;
+    private Double mFieldLengthY;
     private List<Double> mDistances;
     private Double mRelativeCoordinates;
 
-    DataConverter() {
+    DataConverter(Double fieldX, Double fieldY) {
         mDistances = new ArrayList<>();
-    }
-
-    private Double getXPosition() {
-        Double dist1 = mDistances.get(0);
-        Double dist2 = mDistances.get(0);
-        Double dist3 = mDistances.get(0);
-
-        return 0d;
-    }
-
-    private Double getYPosition() {
-        return 0d;
-    }
-
-    protected double calculateDistance(int txPower, double rssi) {
-        if (rssi == 0) {
-            return -1.0; // if we cannot determine distance, return -1.
-        }
-
-        double ratio = rssi * 1.0 / txPower;
-        if (ratio < 1.0) {
-            return Math.pow(ratio, 10);
-        } else {
-            double accuracy = (0.89976) * Math.pow(ratio, 7.7095) + 0.111;
-            return accuracy;
-        }
-    }
-
-    public void setBeaconsCharacteristics(int txPower1, double rssi1, int txPower2, double rssi2, int txPower3, double rssi3) {
-        mDistances.add(calculateDistance(txPower1, rssi1));
-        mDistances.add(calculateDistance(txPower2, rssi2));
-        mDistances.add(calculateDistance(txPower3, rssi3));
+        mFieldLengthX = fieldX;
+        mFieldLengthY = fieldY;
     }
 
     @Override
     public JSONable setBeaconCoordinates(List<Map<Integer, Double>> listOfTxesAndRssis) {
-        return null;
+        Random random = new Random();
+        return new Coordinates(random.nextDouble(), random.nextDouble());
+    }
+
+    private double calculateDistance(int txPower, double rssi) {
+        return 0;
     }
 }
